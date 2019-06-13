ed466926558b162d98d6513826be55e970f00de8
pyatkinmv <pyatkinmv@gmail.com>
2019-06-13 22:08:06 +0300

    First commit

---
+++ app/src/main/java/ru/pyatkinmv/walletApplication/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.pyatkinmv.walletApplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
